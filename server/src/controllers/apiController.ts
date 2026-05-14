3164c61ff1d2fc93afac90279eb78197118046af
Michael W. Farnsworth <stu67810@bisd.org>
2026-05-14 09:21:40 -0500

    Single User

--- server/src/controllers/apiController.ts
+++
@@ -1,1 +0,0 @@
-const getTeamInfo = (req: Request, res: Response) => {};
--- server/src/routes/apiRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { getTeamInfo } from '../api/api.js';
-
-const { Router } = require('express');
-
-const router = Router();
-
-router.get('/teaminfo/:id', getTeamInfo);
